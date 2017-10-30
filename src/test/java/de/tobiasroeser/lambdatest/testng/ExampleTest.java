176532b9bbac591d4286612abfd18ce3b43d600d
Tobias Roeser <le.petit.fou@web.de>
2017-10-30 10:23:48 +0100

    Updates docs and added doc'ed SimpleTest to test suite

---
+++ src/test/java/de/tobiasroeser/lambdatest/testng/ExampleTest.java
@@ -0,0 +1,29 @@
+package de.tobiasroeser.lambdatest.testng;
+
+import static de.tobiasroeser.lambdatest.Expect.*;
+//You can also use JUnit based tests with 
+//import de.tobiasroeser.lambdatest.junit.FreeSpec; 
+import de.tobiasroeser.lambdatest.testng.FreeSpec;
+
+class SimpleTest extends FreeSpec {
+	public SimpleTest() {
+
+		test("1 + 1 = 2", () -> {
+			expectEquals(1 + 1, 2);
+		});
+
+		test("a pending test", () -> pending());
+
+		test("divide by zero", () -> {
+			int a = 2;
+			int b = 0;
+			intercept(ArithmeticException.class, () -> {
+				int c = a / b;
+			});
+		});
+
+		test("demo of a fail", () -> {
+			expectEquals("yes and no", "yes");
+		});
+	}
+}
