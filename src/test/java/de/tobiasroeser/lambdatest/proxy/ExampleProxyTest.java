dea77c764841704b8b535fbbc19149a24e4865f6
Tobias Roeser <le.petit.fou@web.de>
2018-06-11 10:06:54 +0200

    Added documentation for TestProxy

---
+++ src/test/java/de/tobiasroeser/lambdatest/proxy/ExampleProxyTest.java
@@ -0,0 +1,59 @@
+package de.tobiasroeser.lambdatest.proxy;
+
+import static de.tobiasroeser.lambdatest.Expect.expectEquals;
+
+import de.tobiasroeser.lambdatest.testng.FreeSpec;
+
+public class ExampleProxyTest extends FreeSpec {
+
+	interface Dependency {
+		String hello();
+	}
+
+	class ServiceWithDependency {
+		private Dependency dependency;
+
+		public ServiceWithDependency(final Dependency dependency) {
+			this.dependency = dependency;
+		}
+
+		String usingDependency() {
+			return dependency.hello();
+		}
+
+		String notUsingDependency() {
+			return "Have a nice day!";
+		}
+	}
+
+	public ExampleProxyTest() {
+
+			test("A proxy without delegates as optional dependencies should be sufficient", () -> {
+				final Dependency dep = TestProxy.proxy(Dependency.class);
+				final ServiceWithDependency service = new ServiceWithDependency(dep);
+				expectEquals(service.notUsingDependency(), "Have a nice day!");
+			});
+			
+			test("A proxy without delegates as mandatory dependencies should fail", () -> {
+				final Dependency dep = TestProxy.proxy(Dependency.class);
+				final ServiceWithDependency service = new ServiceWithDependency(dep);
+				intercept(UnsupportedOperationException.class, () -> {
+					service.usingDependency();
+				});
+			});
+			
+			test("A proxy with delegates as mandatory dependency should succeed", () -> {
+				final Dependency dep = TestProxy.proxy(Dependency.class, new Object() {
+					@SuppressWarnings("unused")
+					public String hello() {
+						return "Hello Proxy!";
+					}
+				});
+				final ServiceWithDependency service = new ServiceWithDependency(dep);
+				expectEquals(service.usingDependency(), "Hello Proxy!");
+			});
+			
+
+	}
+
+}
