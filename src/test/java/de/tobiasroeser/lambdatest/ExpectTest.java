c0090ff5204d1a35f44e872252c89bfd7d6c576f
ari <info@iba-cg.de>
2018-06-17 16:16:00 +0200

    added tests for exepct overloads

--- src/test/java/de/tobiasroeser/lambdatest/ExpectTest.java
+++ src/test/java/de/tobiasroeser/lambdatest/ExpectTest.java
@@ -1,14 +1,14 @@
 package de.tobiasroeser.lambdatest;
 
+import java.util.Collections;
+
+import org.testng.annotations.Test;
 import static de.tobiasroeser.lambdatest.Intercept.intercept;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
-import org.testng.annotations.Test;
-
 /**
  * We assume {@link Intercept} is properly tested.
- *
  */
 public class ExpectTest {
 
@@ -19,7 +19,7 @@ public class ExpectTest {
 		Expect.expectEquals(1, 1, "ONE");
 	}
 
-	@Test(dependsOnGroups = { "intercept" })
+	@Test(dependsOnGroups = {"intercept"})
 	public void testBaseFailure() throws Exception {
 		Expect.clear();
 		assertEquals(Expect.__TEST_threadContext(), null);
@@ -49,7 +49,7 @@ public class ExpectTest {
 		assertEquals(Expect.__TEST_threadContext(), null);
 	}
 
-	@Test(dependsOnGroups = { "intercept" })
+	@Test(dependsOnGroups = {"intercept"})
 	public void testOneFailingAssert() throws Exception {
 		Expect.clear();
 		assertEquals(Expect.__TEST_threadContext(), null);
@@ -74,7 +74,7 @@ public class ExpectTest {
 		assertEquals(Expect.__TEST_threadContext(), null);
 	}
 
-	@Test(dependsOnGroups = { "intercept" })
+	@Test(dependsOnGroups = {"intercept"})
 	public void testTwoFailingAssert() throws Exception {
 		Expect.clear();
 		assertEquals(Expect.__TEST_threadContext(), null);
@@ -103,7 +103,7 @@ public class ExpectTest {
 		assertEquals(Expect.__TEST_threadContext(), null);
 	}
 
-	@Test(dependsOnGroups = { "intercept" })
+	@Test(dependsOnGroups = {"intercept"})
 	public void testExpectEqualsStringArrayFailEarly() throws Exception {
 		Expect.clear();
 		assertEquals(Expect.__TEST_threadContext(), null);
@@ -111,45 +111,45 @@ public class ExpectTest {
 		assertNotEquals(Expect.__TEST_threadContext(), null);
 
 		Expect.expectEquals(null, null);
-		Expect.expectEquals(new String[] {}, new String[] {});
-		Expect.expectEquals(new String[] { "a" }, new String[] { "a" });
+		Expect.expectEquals(new String[]{}, new String[]{});
+		Expect.expectEquals(new String[]{"a"}, new String[]{"a"});
 		intercept(AssertionError.class, "\\QExpected an array, but got a java.lang.String\\E", () -> {
-			Expect.expectEquals("a", new String[] { "a" });
+			Expect.expectEquals("a", new String[]{"a"});
 		});
 		intercept(AssertionError.class, "\\QGot an array, but did not expected one. Expected a java.lang.String\\E",
 				() -> {
-					Expect.expectEquals(new String[] { "a" }, "a");
+					Expect.expectEquals(new String[]{"a"}, "a");
 				});
 		intercept(AssertionError.class,
 				"\\QActual array length of 1 does not match expected length of 0. Expected [] but was [a]\\E", () -> {
-					Expect.expectEquals(new String[] { "a" }, new String[] {});
+					Expect.expectEquals(new String[]{"a"}, new String[]{});
 				});
 		intercept(AssertionError.class,
 				"\\QActual array length of 0 does not match expected length of 1. Expected [a] but was []\\E", () -> {
-					Expect.expectEquals(new String[] {}, new String[] { "a" });
+					Expect.expectEquals(new String[]{}, new String[]{"a"});
 				});
 		intercept(AssertionError.class,
 				"\\QActual array length of 1 does not match expected length of 2. Expected [a,b] but was [a]\\E",
 				() -> {
-					Expect.expectEquals(new String[] { "a" }, new String[] { "a", "b" });
+					Expect.expectEquals(new String[]{"a"}, new String[]{"a", "b"});
 				});
 		intercept(AssertionError.class,
 				"\\QActual array length of 2 does not match expected length of 1. Expected [a] but was [a,b]\\E",
 				() -> {
-					Expect.expectEquals(new String[] { "a", "b" }, new String[] { "a" });
+					Expect.expectEquals(new String[]{"a", "b"}, new String[]{"a"});
 				});
 		intercept(
 				AssertionError.class,
 				"\\QArrays differ at index 0. Expected [b] but was [a]. Error for element at index 0: Strings differ at index 0 (see [*] marker). Expected \"[*]b\" but was \"[*]a\".\\E",
 				() -> {
-					Expect.expectEquals(new String[] { "a" }, new String[] { "b" });
+					Expect.expectEquals(new String[]{"a"}, new String[]{"b"});
 				});
 
 		Expect.finish();
 		assertEquals(Expect.__TEST_threadContext(), null);
 	}
 
-	@Test(dependsOnGroups = { "intercept" })
+	@Test(dependsOnGroups = {"intercept"})
 	public void testExpectEqualsStringArrayNotFailEarly() throws Exception {
 		Expect.clear();
 		assertEquals(Expect.__TEST_threadContext(), null);
@@ -157,15 +157,15 @@ public class ExpectTest {
 		assertNotEquals(Expect.__TEST_threadContext(), null);
 
 		Expect.expectEquals(null, null);
-		Expect.expectEquals(new String[] {}, new String[] {});
-		Expect.expectEquals(new String[] { "a" }, new String[] { "a" });
-		Expect.expectEquals("a", new String[] { "a" });
-		Expect.expectEquals(new String[] { "a" }, "a");
-		Expect.expectEquals(new String[] { "a" }, new String[] {});
-		Expect.expectEquals(new String[] {}, new String[] { "a" });
-		Expect.expectEquals(new String[] { "a" }, new String[] { "a", "b" });
-		Expect.expectEquals(new String[] { "a", "b" }, new String[] { "a" });
-		Expect.expectEquals(new String[] { "a" }, new String[] { "b" });
+		Expect.expectEquals(new String[]{}, new String[]{});
+		Expect.expectEquals(new String[]{"a"}, new String[]{"a"});
+		Expect.expectEquals("a", new String[]{"a"});
+		Expect.expectEquals(new String[]{"a"}, "a");
+		Expect.expectEquals(new String[]{"a"}, new String[]{});
+		Expect.expectEquals(new String[]{}, new String[]{"a"});
+		Expect.expectEquals(new String[]{"a"}, new String[]{"a", "b"});
+		Expect.expectEquals(new String[]{"a", "b"}, new String[]{"a"});
+		Expect.expectEquals(new String[]{"a"}, new String[]{"b"});
 
 		intercept(AssertionError.class, "\\Q7 expectations failed\\E[\\s\\S]*", () -> {
 			Expect.finish();
@@ -190,14 +190,35 @@ public class ExpectTest {
 	@Test
 	public void testAssertEqualsByteArray() throws Exception {
 		assertEquals((Object) null, (Object) null);
-		assertEquals(new byte[] {}, new byte[] {});
-		assertEquals(new byte[] {}, new Byte[] {});
-		assertEquals(new Byte[] {}, new byte[] {});
-		assertEquals(new Byte[] {}, new Byte[] {});
+		assertEquals(new byte[]{}, new byte[]{});
+		assertEquals(new byte[]{}, new Byte[]{});
+		assertEquals(new Byte[]{}, new byte[]{});
+		assertEquals(new Byte[]{}, new Byte[]{});
 
 		final byte[] a = new byte[3];
 		final byte[] b = new byte[3];
 		Expect.expectEquals(a, b);
 	}
 
+
+	@Test
+	public void testExpectString() {
+		Expect.expectString("abc").hasLength(3);
+	}
+
+	@Test
+	public void testExpectCollection() {
+		Expect.expectCollection(Collections.singleton("abc")).hasSize(1);
+
+	}
+
+	@Test
+	public void testExpectOverloadForString() {
+		Expect.expect("abc").hasLength(3);
+	}
+
+	@Test
+	public void testExpectOverloadForCollection() {
+		Expect.expect(Collections.singleton("abc")).hasSize(1);
+	}
 }
