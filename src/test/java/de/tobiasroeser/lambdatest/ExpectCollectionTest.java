a9d7521c2d303bc66555ec64bad828bd4dad9bdd
ari <info@iba-cg.de>
2018-06-17 15:35:36 +0200

    Cleanup of ExpectCollectionTest tests and fixed all warnings from intellij.

--- src/test/java/de/tobiasroeser/lambdatest/ExpectCollectionTest.java
+++ src/test/java/de/tobiasroeser/lambdatest/ExpectCollectionTest.java
@@ -4,29 +4,22 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import de.tobiasroeser.lambdatest.testng.FreeSpec;
-import org.testng.annotations.Test;
 import static de.tobiasroeser.lambdatest.ExpectCollection.expectCollection;
-import static org.testng.Assert.assertThrows;
 
 public class ExpectCollectionTest extends FreeSpec {
 
 	public ExpectCollectionTest() {
 		setExpectFailFast(true);
 
-		section("ExpectCollection size tests", () -> {
-			test("empty collection", () -> expectCollection(Arrays.asList()).isEmpty());
-			test("test for correct size", () -> expectCollection(Arrays.asList(1)).hasSize(1));
-		});
-
 		section("ExpectCollection.isEmpty", () -> {
-			test("for empty collection", () -> expectCollection(Arrays.asList()).isEmpty());
-			testFail("for non-empty collection should fail", () -> expectCollection(Arrays.asList("1")).isEmpty());
+			test("for empty collection", () -> expectCollection(Collections.emptyList()).isEmpty());
+			testFail("for non-empty collection should fail", () -> expectCollection(Collections.singletonList("1")).isEmpty());
 		});
 
 		section("ExpectCollection.hasSize", () -> {
 			test("for empty collection", () -> expectCollection(Collections.emptyList()).hasSize(0));
 			test("non-empty collection", () -> expectCollection(Arrays.asList(1, 2, 3)).hasSize(3));
-			testFail("wrong size should fail", () -> expectCollection(Arrays.asList("1")).hasSize(2));
+			testFail("wrong size should fail", () -> expectCollection(Collections.singletonList("1")).hasSize(2));
 		});
 
 		section("ExpectCollection.contains", () -> {
@@ -45,19 +38,17 @@ public class ExpectCollectionTest extends FreeSpec {
 		});
 
 		section("ExpectCollection.containsNot", () -> {
-			test("empty collection contains nothing", () -> expectCollection(Arrays.asList()).containsNot(1));
-			testFail("should fail for contained elements", () -> expectCollection(Arrays.asList(1)).containsNot(1));
+			test("empty collection contains nothing", () -> expectCollection(Collections.emptyList()).containsNot(1));
+			testFail("should fail for contained elements", () -> expectCollection(Collections.singletonList(1)).containsNot(1));
 
-			test("[1,2,3] contains 1, 2 and 3", () -> {
-				expectCollection(Arrays.asList(1, 2, 3)).containsNot(0);
-			});
+			test("[1,2,3] contains 1, 2 and 3", () -> expectCollection(Arrays.asList(1, 2, 3)).containsNot(0));
 			test("[a,b,c] contains a, b and c", () -> expectCollection(Arrays.asList("a", "b", "c")).containsNot("x"));
 		});
 
 		section("ExpectCollection.hasNoDuplicates", () -> {
-			test("empty list has no duplicates", () -> expectCollection(Arrays.asList()).hasNoDuplicates());
+			test("empty list has no duplicates", () -> expectCollection(Collections.emptyList()).hasNoDuplicates());
 			test("[1,2,3] has no duplicates", () -> expectCollection(Arrays.asList(1, 2, 3)).hasNoDuplicates());
-			testFail("for collection with dulicates should fail", () -> {
+			testFail("for collection with duplicates should fail", () -> {
 				expectCollection(Arrays.asList(1, 2, 1)).hasNoDuplicates();
 				expectCollection(Arrays.asList(2, 2)).hasNoDuplicates();
 			});
