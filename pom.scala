ab299220e42c0f1915ad1b19b0ba0f67b8306393
Tobias Roeser <le.petit.fou@web.de>
2018-07-18 14:35:27 +0200

    Version bump to polyglot-scala 0.3.1

--- pom.scala
+++ pom.scala
@@ -21,7 +21,7 @@ object Plugins {
   val jar = "org.apache.maven.plugins" % "maven-jar-plugin" % "2.5"
   val retrolambda = "net.orfjackal.retrolambda" % "retrolambda-maven-plugin" % "1.8.0"
   val reproducibleBuild = "io.github.zlika" % "reproducible-build-maven-plugin" % "0.7"
-  val translate = "io.takari.polyglot" % "polyglot-translate-plugin" % "0.3.0"
+  val translate = "io.takari.polyglot" % "polyglot-translate-plugin" % "0.3.1"
   val javadoc = "org.apache.maven.plugins" % "maven-javadoc-plugin" % "3.0.1"
 }
 
