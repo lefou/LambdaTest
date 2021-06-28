679cd83259cf50c2a4a2aef8fbbe0d300459839e
Tobias Roeser <le.petit.fou@web.de>
2021-06-28 15:21:27 +0200

    Prepared release 0.7.1

--- pom.scala
+++ pom.scala
@@ -2,7 +2,7 @@ import org.sonatype.maven.polyglot.scala.model._
 import scala.collection.immutable.Seq
 
 val namespace = "de.tobiasroeser.lambdatest"
-val lambdatest = "de.tototec" % namespace % "0.7.1-SNAPSHOT"
+val lambdatest = "de.tototec" % namespace % "0.7.1"
 val gav = lambdatest
 val url = "https://github.com/lefou/LambdaTest"
 
